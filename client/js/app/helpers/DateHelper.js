09549ccd6eed281c2adaabaf3623f55e3ef7e24d
Wizarion <ed.rabelo@gmail.com>
2018-05-02 23:32:08 -0300

    Final da lição

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -9,6 +9,10 @@ class DateHelper {
     }
 
     static textoParaData(texto) {
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato yyyy-MM-dd');
+        
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
