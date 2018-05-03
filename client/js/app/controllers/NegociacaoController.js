a131fa76e3d1da74dd5dd82c81551449f59b934f
Wizarion <ed.rabelo@gmail.com>
2018-05-02 22:57:07 -0300

    first commit

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,22 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let negociacao = new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(DateHelper.dataParaTexto(negociacao.data));
+    }
+}
